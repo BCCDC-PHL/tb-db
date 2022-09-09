e39ea7321089379956fbae1cf32a1c680f067359
Anders Goncalves da Silva <andersgs@gmail.com>
2022-09-09 11:39:58 -0700

    Add pytest dev dependency

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup, find_packages
 
 
-setup(name='tb_db',
-      version='0.1.0',
-      packages=find_packages(),
-      scripts=[],
-      package_data={},
-      install_requires=[
-            "psycopg2-binary==2.9.3",
-            "sqlalchemy==1.4.40",
-            "alembic==1.8.0"
-        ],
-      description='',
-      url='',
-      author='',
-      author_email='',
-      include_package_data=True,
-      keywords=[],
-      zip_safe=False
+setup(
+    name="tb_db",
+    version="0.1.0",
+    packages=find_packages(),
+    scripts=[],
+    package_data={},
+    install_requires=["psycopg2-binary==2.9.3", "sqlalchemy==1.4.40", "alembic==1.8.0"],
+    description="",
+    url="",
+    author="",
+    author_email="",
+    include_package_data=True,
+    keywords=[],
+    zip_safe=False,
+    extra_requires={
+        "dev": [
+            "pytest>=7.1.2",
+        ]
+    },
 )
