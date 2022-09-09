ee279903f203ba7899799667e962bd23de619801
Anders Goncalves da Silva <andersgs@gmail.com>
2022-09-09 11:40:46 -0700

    Refactor to add row versioning with timestamps

--- tb_db/model.py
+++ tb_db/model.py
@@ -6,11 +6,15 @@ from sqlalchemy import String
 from sqlalchemy import Date
 from sqlalchemy import DateTime
 from sqlalchemy import JSON
+from sqlalchemy import event
+from sqlalchemy import func
+from sqlalchemy.orm import attributes
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import declared_attr
 from sqlalchemy.orm import declarative_mixin
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
+from sqlalchemy.orm import make_transient
+from sqlalchemy.orm import Session
 
 
 def camel_to_snake(s):
@@ -31,17 +35,55 @@ Base = declarative_base(cls=Base)
 @declarative_mixin
 class HistoryMixin:
     created_at = Column(DateTime, default=func.now())
-    valid_until = Column(DateTime, default=None, onupdate=func.now())
+    valid_until = Column(DateTime, default=None)
+
+    def new_version(self, session):
+        # invalidate current version - generate an update
+        old_id = self.id
+        session.query(self.__class__).filter_by(id=old_id).update(
+            values=dict(valid_until=func.now()), synchronize_session=False
+        )
+
+        # make us transient (removes persistent identity)
+        # make this an INSERT
+        make_transient(self)
+        self.id = None
+
+        # set created_at and valid_until, which means we have a new PK.
+        self.created_at = func.now()
+        self.valid_until = None
+
+
+@event.listens_for(Session, "before_flush")
+def before_flush(session, flush_context, instances):
+    for instance in session.dirty:
+        if not isinstance(instance, HistoryMixin):
+            continue
+        if not session.is_modified(instance):
+            continue
+
+        if not attributes.instance_state(instance).has_identity:
+            continue
+
+        # make it transient
+        instance.new_version(session)
+
+        # re-add
+        session.add(instance)
 
 
 class Sample(Base, HistoryMixin):
 
     sample_id = Column(String)
     collection_date = Column(Date)
-    cgmlst_cluster_id = Column(Integer, ForeignKey("cgmlst_cluster.id"), nullable=True)
+    cgmlst_cluster_id = Column(
+        Integer,
+        ForeignKey("cgmlst_cluster.id"),
+        nullable=True,
+    )
     miru_cluster_id = Column(Integer, ForeignKey("miru_cluster.id"), nullable=True)
 
-    cgmlst_cluster = relationship("CgMlstCluster", back_populates="samples")
+    cgmlst_cluster = relationship("CgmlstCluster", back_populates="samples")
     miru_cluster = relationship("MiruCluster", back_populates="samples")
 
 
